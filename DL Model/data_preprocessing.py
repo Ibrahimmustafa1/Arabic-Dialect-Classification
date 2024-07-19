577aa7f238bfd7737150191f2e2aad93611bc2aa
Ibrahim Mustafa <85252957+Ibrahimmustafa1@users.noreply.github.com>
2024-07-19 16:54:35 +0300

    Update data_preprocessing.py

--- DL Model/data_preprocessing.py
+++ DL Model/data_preprocessing.py
@@ -3,6 +3,8 @@
 import re
 from nltk.corpus import stopwords
 import nltk
+from nltk.tokenize import word_tokenize
+
 nltk.download('stopwords')
 nltk.download('punkt')
 
@@ -10,42 +12,67 @@ arabic_stopwords = set(stopwords.words('arabic'))
 
 def remove_emojis(text):
     emoji_pattern = re.compile(
-        "["  # Start of the pattern
-        "\U0001F600-\U0001F64F"  # emoticons
-        "\U0001F300-\U0001F5FF"  # symbols & pictographs
-        "\U0001F680-\U0001F6FF"  # transport & map symbols
-        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        "\U00002500-\U00002BEF"  # chinese char
-        "\U00002702-\U000027B0"
-        "\U00002702-\U000027B0"
-        "\U000024C2-\U0001F251"
-        "\U0001f926-\U0001f937"
-        "\U00010000-\U0010ffff"
-        "\u2640-\u2642"
-        "\u2600-\u2B55"
-        "\u200d"
-        "\u23cf"
-        "\u23e9"
-        "\u231a"
-        "\u3030"
-        "\ufe0f"
-        "\u2069"
-        "\u2066"
-        "\u200c"
-        "\u2067"
-        "\u2068"
-        "]+",
-        flags=re.UNICODE,
+        "["                       
+        "\U0001F600-\U0001F64F"  
+        "\U0001F300-\U0001F5FF"  
+        "\U0001F680-\U0001F6FF"  
+        "\U0001F1E0-\U0001F1FF"  
+        "\U00002700-\U000027BF"  
+        "\U000024C2-\U0001F251"  
+        "]+", flags=re.UNICODE   
     )
-    return emoji_pattern.sub(r"", text)
+    return emoji_pattern.sub(r'', text)
+
+def normalize_arabic(text):
+    text = re.sub("[إأآا]", "ا", text)
+    text = re.sub("ى", "ي", text)
+    text = re.sub("ؤ", "و", text)
+    text = re.sub("ئ", "ي", text)
+    text = re.sub("ة", "ه", text)
+    return text
+
+def remove_diacritics(text):
+    arabic_diacritics = re.compile("""
+                             ّ    | # Tashdid
+                             َ    | # Fatha
+                             ً    | # Tanwin Fath
+                             ُ    | # Damma
+                             ٌ    | # Tanwin Damm
+                             ِ    | # Kasra
+                             ٍ    | # Tanwin Kasr
+                             ْ    | # Sukun
+                             ـ     # Tatwil/Kashida
+                         """, re.VERBOSE)
+    return re.sub(arabic_diacritics, '', text)
+
+def remove_mentions(text):
+    return re.sub(r'@\w+', '', text)
+
+def remove_hashtags(text):
+    return re.sub(r'#\w+', '', text)
+
+def remove_urls(text):
+    return re.sub(r'http\S+', '', text)
+
+def remove_non_arabic(text):
+    return re.sub(r'[^\u0621-\u064A\s]', '', text)
+
+def remove_english(text):
+    return re.sub(r'[a-zA-Z]', '', text)
 
 def preprocess_text(text):
     text = remove_emojis(text)
-    text = text.lower()
-    text = re.sub(r'\d+', '', text)  # remove numbers
-    text = re.sub(r'\s+', ' ', text)  # remove extra whitespace
-    text = text.strip()
-    return text
+    text = remove_mentions(text)
+    text = remove_hashtags(text)
+    text = remove_urls(text)
+    text = remove_non_arabic(text)
+    text = remove_english(text)
+    text = normalize_arabic(text)
+    text = re.sub(r'[^\w\s]', '', text)
+    text = remove_diacritics(text)
+    words = word_tokenize(text)
+    words = [word for word in words if word not in arabic_stopwords]
+    return ' '.join(words)
 
 def preprocess_dataframe(df):
     df['text'] = df['text'].apply(preprocess_text)
